324c6c42e8342300555bac960832ab624b00ab62
LAPTOP-UAKE3R97\aans <aans@live.dk>
2022-11-15 09:09:13 +0100

    Chop trees, open close fence

--- Assets/Hit.cs
+++ Assets/Hit.cs
@@ -9,13 +9,23 @@ public class Hit : MonoBehaviour
     public GameObject blood;
     public GameObject food;
 
+    //tree
+    public GameObject splinters;
+    public GameObject wood;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Chicken"))
         {
-            
             Instantiate(blood,other.transform.position, other.transform.rotation);
             Instantiate(food, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Tree"))
+        {
+            Instantiate(splinters, other.transform.position, other.transform.rotation);
+            Instantiate(wood, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
         }
     }
